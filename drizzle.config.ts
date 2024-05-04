5c3341fc8c242c409c5878eab039ae6b6d896cad
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-04 11:38:32 -0500

    Run Prettier

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["icons-app_*"],
+   schema: "./src/server/db/schema.ts",
+   driver: "pg",
+   dbCredentials: {
+      connectionString: env.DATABASE_URL,
+   },
+   tablesFilter: ["icons-app_*"],
 } satisfies Config;
