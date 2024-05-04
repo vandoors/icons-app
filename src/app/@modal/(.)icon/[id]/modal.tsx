698e739f04a37203d94488f7b67f2e4e50b2cb02
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-04 15:33:33 -0500

    Update icon view and modal styling

--- src/app/@modal/(.)icon/[id]/modal.tsx
+++ src/app/@modal/(.)icon/[id]/modal.tsx
@@ -21,11 +21,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
    return createPortal(
       <dialog
          ref={dialogRef}
-         className="m-0 h-screen w-screen bg-black/50"
+         className="m-0 h-screen w-screen bg-black/50 text-white backdrop-blur"
          onClose={onDismiss}
       >
          {children}
-         <button onClick={onDismiss} className="close-button" />
+         {/* <button onClick={onDismiss} className="close-button" /> */}
       </dialog>,
       document.getElementById("modal-root")!,
    );
--- src/components/full-icon-page.tsx
+++ src/components/full-icon-page.tsx
@@ -3,5 +3,18 @@ import { getIcon } from "~/server/queries";
 export default async function FullPageIconView(props: { id: number }) {
    const icon = await getIcon(props.id);
 
-   return <img src={icon.url} alt={icon.name} />;
+   return (
+      <div className="flex h-full w-full min-w-0">
+         <div className="flex flex-shrink items-center justify-center">
+            <img
+               src={icon.url}
+               alt={icon.name}
+               className="max-w-2xl object-contain"
+            />
+         </div>
+         <div className="flex w-48 flex-shrink-0 flex-col items-center justify-center">
+            <div className="text-xl font-bold">{icon.name}</div>
+         </div>
+      </div>
+   );
 }
