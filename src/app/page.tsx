5ced40b56fab0158a47c9b9a88879d2ae9570690
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-04 14:36:36 -0500

    Use next/image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { db } from "~/server/db";
 import { getIcons } from "~/server/queries";
@@ -9,10 +10,16 @@ export default async function HomePage() {
 
    return (
       <main>
-         <div className="flex flex-wrap gap-4">
+         <div className="flex flex-wrap justify-center gap-4">
             {icons.map((image) => (
                <div key={image.url} className="w-48">
-                  <img src={image.url} />
+                  <Image
+                     src={image.url}
+                     style={{ objectFit: "contain" }}
+                     width={1024}
+                     height={1024}
+                     alt={image.name}
+                  />
                   <div>{image.name}</div>
                </div>
             ))}
